3a1b152afe7f4320956094f81112f0172987ea27
bd_cloud_kamon <625110949@qq.com>
2019-04-01 15:55:52 +0800

    .

---
+++ common/config/main-ol.php
@@ -0,0 +1,12 @@
+<?php
+	return [
+		'components'=>[
+			'db' => [
+            		'class' => 'yii\db\Connection',
+            		'dsn' => 'mysql:host=106.13.125.248:3306;dbname=bookcenter',
+            		'username' => 'root',
+            		'password' => '!@#123asd',
+            		'charset' => 'utf8',
+        		],	
+		],
+	];
