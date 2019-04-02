1b5449ebce4467171b39295ad738b77fe00379cb
bd_cloud_kamon <625110949@qq.com>
2019-04-02 11:21:54 +0800

    .

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,7 +1,6 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_ENV') or define('YII_ENV', 'prod');
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
@@ -10,9 +9,9 @@ require __DIR__ . '/../config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../../common/config/main.php',
-    require __DIR__ . '/../../common/config/main-ol.php',
+    require __DIR__ . '/../../common/config/main-local.php',
     require __DIR__ . '/../config/main.php',
-    require __DIR__ . '/../config/main-ol.php'
+    require __DIR__ . '/../config/main-local.php'
 );
 
 (new yii\web\Application($config))->run();
