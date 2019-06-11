07a07fdca0b9869affc0f7d46d85c0a03db06efc
liujiawang <625110949@qq.com>
2019-06-11 17:55:02 +0800

    show控制器添加ppt内容

--- frontend/views/show/index.php
+++ frontend/views/show/index.php
@@ -8,14 +8,16 @@ $this->registerCssFile('./css/show.css');
     <div class="main-content">
         <div id="ppt">
             <div class="ppt-content">
-                <div class="ppt-content-block index-bg hide"></div>
-                <div class="ppt-content-block hide">
+                <div class="ppt-content-block first-content index-bg hide"></div>
+                <div class="ppt-content-block second-content hide">
                     <div class="second-content-title">
                         <span>资料袋</span>
                     </div>
                     <div>
-                        <span class="second-content">
-                            &nbsp;&nbsp;长城始建于<span class="text-red">春秋战国</span>（公元前770——公元前476）时期，那时各诸侯国为了互相防御，都在自己的境内地势险要的地方修筑长城。<span class="text-red">秦始皇</span>统一中国以后，为防御北方匈奴贵族的入侵，将各个诸侯国的长城连接起来并将其延长，号称万里长城。<span class="text-red">以后各个朝代，都对长城进行修建。</span><span class="text-blue">因此可以说长城是我国古代人民在兵器还不发达的情况下，为了抵御外族入侵而建筑的军事建筑</span>。
+                        <span class="second-span-block">
+                            &nbsp;&nbsp;长城始建于<span class="text-red">春秋战国</span>（公元前770——公元前476）时期，那时各诸侯国为了互相防御，都在自己的境内地势险要的地方修筑长城。<span
+                                    class="text-red">秦始皇</span>统一中国以后，为防御北方匈奴贵族的入侵，将各个诸侯国的长城连接起来并将其延长，号称万里长城。<span
+                                    class="text-red">以后各个朝代，都对长城进行修建。</span><span class="text-blue">因此可以说长城是我国古代人民在兵器还不发达的情况下，为了抵御外族入侵而建筑的军事建筑</span>。
                         </span>
                     </div>
                 </div>
@@ -88,90 +90,241 @@ $this->registerCssFile('./css/show.css');
                         <span><span class="text-red">崇</span>山<span class="text-red">峻</span>岭</span>
                     </div>
                 </div>
-                <div class="ppt-content-block firth-content">
+                <div class="ppt-content-block firth-content hide">
                     <div class="text-block">
-                        <br />
+                        <br/>
                         <span>盘<span class="text-red">旋</span></span>
                     </div>
                     <div class="text-block">
-                        <br />
+                        <br/>
                         <span><span class="text-red">嘉峪</span>关</span>
                     </div>
                     <div class="text-block">
-                        <br />
+                        <br/>
                         <span>方<span class="text-red">砖</span></span>
                     </div>
                     <div class="text-block">
-                        <br />
+                        <br/>
                         <span>间<span class="text-red">隔</span></span>
                     </div>
                     <div class="text-block">
-                        <br />
+                        <br/>
                         <span><span class="text-red">屯</span>兵</span>
                     </div>
                     <div class="text-block">
-                        <br />
+                        <br/>
                         <span class="text-red">堡垒</span>
                     </div>
                     <div class="text-block">
-                        <br />
+                        <br/>
                         <span>打<span class="text-red">仗</span></span>
                     </div>
                     <div class="text-block">
-                        <br />
+                        <br/>
                         <span><span class="text-red">扶</span>手</span>
                     </div>
                     <div class="text-block">
-                        <br />
+                        <br/>
                         <span class="text-red">智慧</span>
                     </div>
                     <div class="text-block">
-                        <br />
+                        <br/>
                         <span>气<span class="text-red">魄</span></span>
                     </div>
                     <div class="text-block">
-                        <br />
+                        <br/>
                         <span><span class="text-red">瞭</span>望口</span>
                     </div>
                     <div class="text-block">
-                        <br />
+                        <br/>
                         <span><span class="text-red">崇</span>山<span class="text-red">峻</span>岭</span>
                     </div>
                 </div>
-            </div>
-        </div>
-
-            <div class="row">
-                <div class="col-md-4"></div>
-                <div class="col-md-4" align="center">
-                    <ul class="pager">
-                        <li class="previous">
-                            <button class="btn btn-primary" type="button">
-                                <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>
-                                上一页
-                            </button>
-                        </li>
-                        <li class="next">
-                            <button class="btn btn-primary" type="button">
-                                下一页
-                                <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
-                            </button>
-                        </li>
-                    </ul>
+                <div class="ppt-content-block sixth-content show">
+                    <div class="sixty-bg">
+                        <div class="sixty-title">
+                            <span>崇山峻岭</span>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-4"></div>
+                <div class="ppt-content-block seventh-content hide">
+                    <div class="seventh-bg">
+                        <div class="seventh-title">
+                            <span class="text-red">长城给你留下怎样的印象？</span>
+                        </div>
+                        <div class="seventh-left"></div>
+                        <div class="seventh-right"></div>
+                    </div>
+                </div>
+                <div class="ppt-content-block eighth-content hide">
+                    <div class="seventh-bg">
+                        <div class="eighth-title">
+                            <span>这样<span class="text-blue">气魄雄伟</span>的工程，在世界历史上是一个伟大的<span class="text-red">奇迹</span>。</span>
+                        </div>
+                        <div class="seventh-left"></div>
+                        <div class="seventh-right"></div>
+                    </div>
+                </div>
+                <div class="ppt-content-block ninth-content hide">
+                    <div style="margin-top: 50px"></div>
+                    <div class="second-content-title">
+                        <span>默读要求：</span>
+                    </div>
+                    <div class="size-44-black" align="left">
+                        &nbsp;&nbsp;&nbsp;&nbsp;默读课文1、2自然段，思考问题： 你从哪些句子中感受到长城气魄雄伟？ 请用“——”画出来。
+                    </div>
+                </div>
+                <div class="ppt-content-block tenth-content hide">
+                    <div class="tenth-bg">
+                        <div class="tenth-title" align="left">
+                            <span class="text-orange">&nbsp;&nbsp;&nbsp;&nbsp;远看长城，像一条<span class="text-red">长龙</span>，在崇山峻岭之间蜿蜒盘旋。</span>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="ppt-content-block eleventh-content hide">
+                    <div class="eleventh-bg"></div>
+                    <div style="margin-top: 20px;" align="left">
+                        <span class="size-44-black">
+                            &nbsp;&nbsp;&nbsp;&nbsp;远看长城，它像一条长龙，在崇山峻岭之间蜿蜒盘旋。
+                            <span class="text-red">从东头的山海关到西头的嘉峪关，有一万三千多里。</span>
+                        </span>
+                    </div>
+                </div>
+                <div class="ppt-content-block twelfth-content hide">
+                    <div class="twelfth-span-block" align="left">
+                        <span class="text-red">&nbsp;&nbsp;&nbsp;&nbsp;长城位于中国北部，它东起河北省渤海湾的山海关，西至内陆地区甘肃省的嘉峪关。横贯河北、北京、内蒙古、山西、陕西、宁夏、甘肃等数个省、市、自治区，全长约6700公里，约13300里，在世上有“万里长城”之誉。</span>
+                    </div>
+                    <div class="twelfth-bg"></div>
+                </div>
+                <div class="ppt-content-block thirteenth-content hide">
+                    <div style="margin-top: 30px;"></div>
+                    <div class="twelfth-span-block">
+                        <span class="text-red size-44-black">&nbsp;&nbsp;&nbsp;&nbsp;前不见头，后不见尾。</span>
+                    </div>
+                    <div class="twelfth-bg"></div>
+                </div>
+                <div class="ppt-content-block fourteenth-content hide">
+                    <div class="fourteenth-bg"></div>
+                    <div style="margin-top: 20px;" align="left">
+                        <span class="size-44-black">
+                            &nbsp;&nbsp;&nbsp;&nbsp;远看长城，它像一条长龙，在崇山峻岭之间蜿蜒盘旋。
+                            <span class="text-red">从东头的山海关到西头的嘉峪关，有一万三千多里。</span>
+                        </span>
+                    </div>
+                </div>
+                <div class="ppt-content-block fifteenth-content hide">
+                    <div class="fifteenth-img" align="center">
+                        <div class="shupai">
+                            <span class="size-44-black text-blue">远<br/>看</span>
+                        </div>
+                        <img src="../image/cs5.png"/>
+                    </div>
+                    <div class="fifteenth-img" align="center">
+                        <div class="shupai">
+                            <span class="size-44-black text-blue">近<br/>看</span>
+                        </div>
+                        <img src="../image/cs6.png"/>
+                    </div>
+                </div>
+                <div class="ppt-content-block sixteenth-content hide">
+                    <div class="sixteenth-span-block">
+                        <span class="size-44-black">
+                            &nbsp;&nbsp;&nbsp;&nbsp;这一段长城修筑在八达岭上，<span class="text-red">高大坚固</span>，是用巨大的条石和城砖筑成的。
+                        </span>
+                    </div>
+                </div>
+                <div class="ppt-content-block seventeenth-content hide">
+                    <div class="seventeenth-span-block" align="left">
+                        <span class="size-44-black">&nbsp;&nbsp;&nbsp;&nbsp;城墙顶上铺着方砖，十分平整，像很宽的马路，五六匹马可以并行。</span>
+                    </div>
+                    <div class="seventeenth-img"></div>
+                </div>
+                <div class="ppt-content-block eighteenth-content hide">
+                    <div class="eighteenth-span-block" align="left">
+                        <span>
+                            &nbsp;&nbsp;&nbsp;&nbsp;城墙外沿有两米多高的成排的<span class="text-red">垛子</span>,垛子上有方形的<span
+                                    class="text-red">瞭望口</span>和<span
+                                    class="text-red">射口</span>,供瞭望和射击用。城墙顶上,每隔三百米就有一座<span class="text-red">方形的城台</span>,是屯兵的堡垒。打仗的时候,城台之间可以互相呼应。
+                        </span>
+                    </div>
+
+                </div>
+                <div class="ppt-content-block nineteenth-content hide">
+                    <div class="nineteenth-span-block">
+                        <span>
+                            &nbsp;&nbsp;&nbsp;&nbsp;从北京出发，不过一百多里就来到长城脚下。这一段长城修筑在八达岭上，高大坚固，是用巨大的条石和城砖筑成的。城墙顶上铺着方砖，十分平整，像很宽的马路，五六匹马可以并行。城墙外沿有两米多高的成排的垛子，垛子上有方形的瞭望口和射口，供瞭望和射击用。城墙顶上，每隔三百多米就有一座方形的城台，是屯兵的堡垒。打仗的时候，城台之间可以互相呼应。
+                        </span>
+                    </div>
+                    <div class="nineteenth-img"></div>
+                </div>
+                <div class="ppt-content-block twentieth-content hide">
+                    <p>
+                        四、拓展阅读
+                    </p>
+                    <br/>
+                    <p>
+                        1.必读：《陆水河畔》
+                    </p>
+                    <br/>
+                    <p>
+                        2.选读：《仰望布达拉》
+                        <br/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        《孟姜女哭长城传说》
+                    </p>
+                </div>
+                <div class="ppt-content-block twentieth-content hide">
+                    <div style="margin-top: 60px;"></div>
+                    <p>
+                        五、写话训练
+
+                    </p>
+                    <br/>
+                    <p>
+                        &nbsp;&nbsp;&nbsp;&nbsp;我们经常在校园的操场活动，你能按一定的顺序介绍校园的操场吗？
+                    </p>
+                </div>
+                <div class="ppt-content-block twentieth-content hide" align="center">
+                    <div style="margin-top: 200px;"></div>
+                    <p>
+                        作业：请向你的家长介绍长城。
+                    </p>
+                </div>
             </div>
         </div>
-        <div id="video">
-            <p align="center">
-                <video src="./video/1.mp4" width="640" height="325" controls="controls"></video>
-            </p>
+
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4" align="center">
+                <ul class="pager">
+                    <li class="previous">
+                        <button class="btn btn-primary" type="button">
+                            <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>
+                            上一页
+                        </button>
+                    </li>
+                    <li class="next">
+                        <button class="btn btn-primary" type="button">
+                            下一页
+                            <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+                        </button>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4"></div>
         </div>
+    </div>
+    <div id="video">
+        <p align="center">
+            <video src="./video/1.mp4" width="640" height="325" controls="controls"></video>
+        </p>
+    </div>
 
 
-        <div id="lw">
-            sddd
-        </div>
+    <div id="lw">
+        sddd
+    </div>
     </div>
 
 <?php
--- frontend/web/js/show.js
+++ frontend/web/js/show.js
@@ -1,9 +1,35 @@
 $(function () {
+    var ppt_count = $('.ppt-content').find('.ppt-content-block').length;
     $('.nav-stacked li').click(function () {
         var id = ($(this).attr('data-value'));
         $(this).siblings().removeClass('active');
         $(this).addClass('active');
-        $('#'+id).siblings().hide();
-        $('#'+id).show();
+        $('#' + id).siblings().hide();
+        $('#' + id).show();
     });
+
+
+    //上一页
+    $('.previous').click(function () {
+        var num = $('.ppt-content').find('.show').index();
+        if (num == 0) {
+            alert('当前是第一页');
+            return false;
+        }
+        $(".ppt-content-block").eq(num).removeClass('show');
+        $(".ppt-content-block").eq(num).addClass('hide');
+        $(".ppt-content-block").eq(num - 1).addClass('show');
+    });
+    //下一页
+    $(".next").click(function () {
+        var num = $('.ppt-content').find('.show').index();
+        if(num == (ppt_count-1)){
+            alert("当前是最后一页");
+            return false;
+        }
+        $(".ppt-content-block").eq(num).removeClass('show');
+        $(".ppt-content-block").eq(num).addClass('hide');
+        $(".ppt-content-block").eq(num + 1).addClass('show');
+    });
+
 });
