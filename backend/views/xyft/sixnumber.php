95ee51a870cfbeb757ef54d50c554dc1d48c6fe2
liujw <625110949@qq.com>
2019-08-07 22:40:57 +0800

    modify

--- backend/views/xyft/sixnumber.php
+++ backend/views/xyft/sixnumber.php
@@ -18,6 +18,7 @@ $this->registerCssFile('/css/chance.css');
             <table class="table table-bordered">
                 <thead>
                 <tr>
+                    <td>序号</td>
                     <td>期数</td>
                     <td>开奖结果</td>
                     <td>相隔期数</td>
@@ -25,9 +26,10 @@ $this->registerCssFile('/css/chance.css');
                 </thead>
                 <tbody>
                 <?php
-                foreach($first as $val){
+                foreach($first as $key => $val){
                     ?>
                     <tr>
+                        <td><?= ($key+1) ?></td>
                         <td><?= $val['stage'] ?></td>
                         <td><span class="number num_<?= $val['kj'] ?> "> <?= $val['kj'] ?> </span></td>
                         <td><?= $val['partition'] ?></td>
@@ -51,6 +53,7 @@ $this->registerCssFile('/css/chance.css');
             <table class="table table-bordered">
                 <thead>
                 <tr>
+                    <td>序号</td>
                     <td>期数</td>
                     <td>开奖结果</td>
                     <td>相隔期数</td>
@@ -58,9 +61,10 @@ $this->registerCssFile('/css/chance.css');
                 </thead>
                 <tbody>
                 <?php
-                foreach($ten as $val){
+                foreach($ten as $key => $val){
                     ?>
                     <tr>
+                        <td><?= ($key+1) ?></td>
                         <td><?= $val['stage'] ?></td>
                         <td><span class="number num_<?= $val['kj'] ?> "> <?= $val['kj'] ?> </span></td>
                         <td><?= $val['partition'] ?></td>
