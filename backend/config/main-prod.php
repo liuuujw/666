4aece6090993a3cb334756bc3bbe495323b4c62e
liujw <625110949@qq.com>
2019-09-29 22:01:21 +0800

    modify pwd

--- backend/config/main-prod.php
+++ backend/config/main-prod.php
@@ -17,14 +17,14 @@ $config = [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=106.13.125.248:3306;dbname=book_center',
             'username' => 'root',
-            'password' => 'Dbc!@#106',
+            'password' => 'Bdc!@#106',
             'charset' => 'utf8',
         ],
         'dbol' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=106.13.125.248:3306;dbname=book_center',
             'username' => 'root',
-            'password' => 'Dbc!@#106',
+            'password' => 'Bdc!@#106',
             'charset' => 'utf8',
         ]
     ],
